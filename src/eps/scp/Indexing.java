4ad93716fc76742520c8399b379792fc12ad10f0
albertm15 <155188920+albertm15@users.noreply.github.com>
2026-01-15 19:13:38 +0100

    Update Indexing.java

--- src/eps/scp/Indexing.java
+++ src/eps/scp/Indexing.java
@@ -1,11 +1,3 @@
-/* ---------------------------------------------------------------
-Práctica 3.
-Código fuente : IndiceInvertidoConcPRA3
-Grado Informática
-49381774S Albert Martín López.
-49380060A Pau Barahona Setó.
---------------------------------------------------------------- */
-
 
 package eps.scp;
 
